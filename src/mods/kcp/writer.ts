5025f4a5c1a573688419e0faed945a8dc972778b
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-22 16:32:40 +0200

    fixes

--- src/mods/kcp/writer.ts
+++ src/mods/kcp/writer.ts
@@ -2,7 +2,7 @@ import { Writable } from "@hazae41/binary";
 import { SuperTransformStream } from "@hazae41/cascade";
 import { None, Some } from "@hazae41/option";
 import { AbortError, CloseError, ErrorError, Plume, StreamEvents, SuperEventTarget } from "@hazae41/plume";
-import { Ok, Result } from "@hazae41/result";
+import { Err, Ok, Result } from "@hazae41/result";
 import { KcpSegment } from "./segment.js";
 import { SecretKcpDuplex } from "./stream.js";
 
@@ -12,8 +12,6 @@ export class SecretKcpWriter {
 
   readonly stream: SuperTransformStream<Writable, Writable>
 
-  closed = false
-
   constructor(
     readonly parent: SecretKcpDuplex,
   ) {
@@ -22,42 +20,45 @@ export class SecretKcpWriter {
     })
   }
 
-  async #onWrite(fragment: Writable): Promise<Result<void, AbortError | ErrorError | CloseError>> {
-    if (this.stream.closed) return Ok.void()
+  async #onWrite(fragment: Writable): Promise<Result<void, unknown | AbortError | ErrorError | CloseError>> {
+    return await Result.unthrow(async t => {
+      if (this.stream.closed) // TODO: check if we can remove this
+        return new Err(new CloseError({ cause: this.stream.closed.reason }))
 
-    const conversation = this.parent.conversation
-    const command = KcpSegment.commands.push
-    const serial = this.parent.send_counter++
-    const unackSerial = this.parent.recv_counter
+      const conversation = this.parent.conversation
+      const command = KcpSegment.commands.push
+      const serial = this.parent.send_counter++
+      const unackSerial = this.parent.recv_counter
 
-    const segment = KcpSegment.tryNew({ conversation, command, serial, unackSerial, fragment }).unwrap()
+      const segment = KcpSegment.tryNew({ conversation, command, serial, unackSerial, fragment }).throw(t)
 
-    this.stream.enqueue(segment)
-
-    const start = Date.now()
-
-    const retry = setInterval(() => {
-      if (this.stream.closed) {
-        clearInterval(retry)
-        return
-      }
-
-      const delay = Date.now() - start
-      console.debug(`Retrying KCP after`, delay, `milliseconds`)
       this.stream.enqueue(segment)
-    }, 1000)
 
-    const signal = AbortSignal.timeout(60 * 1000)
+      const start = Date.now()
 
-    const result = await Plume.tryWaitStream(this.parent.reader.events, "ack", segment => {
-      if (segment.serial !== serial)
-        return new Ok(new None())
-      return new Ok(new Some(Ok.void()))
-    }, signal)
+      const retry = setInterval(() => {
+        if (this.stream.closed) {
+          clearInterval(retry)
+          return
+        }
 
-    clearInterval(retry)
+        const delay = Date.now() - start
+        console.debug(`Retrying KCP after`, delay, `milliseconds`)
+        this.stream.enqueue(segment)
+      }, 1000)
 
-    return result
+      const signal = AbortSignal.timeout(60 * 1000)
+
+      const result = await Plume.tryWaitStream(this.parent.reader.events, "ack", segment => {
+        if (segment.serial !== serial)
+          return new Ok(new None())
+        return new Ok(new Some(Ok.void()))
+      }, signal)
+
+      clearInterval(retry)
+
+      return result
+    })
   }
 
 }
