156af8b8620ae7d10ea88f1bfba7056110bdf028
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-24 21:37:32 +0200

    ignore

--- src/mods/kcp/stream.ts
+++ src/mods/kcp/stream.ts
@@ -59,12 +59,14 @@ export class SecretKcpDuplex {
       .pipeTo(read.writable)
       .then(this.#onReadClose.bind(this))
       .catch(this.#onReadError.bind(this))
+      .then(r => r.ignore())
       .catch(console.error)
 
     write.readable
       .pipeTo(stream.writable)
       .then(this.#onWriteClose.bind(this))
       .catch(this.#onWriteError.bind(this))
+      .then(r => r.ignore())
       .catch(console.error)
   }
 
