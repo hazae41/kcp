c78e074b86c31b8baf50caa4d2e4a7e9aeeaacd6
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-11-15 14:29:46 +0100

    fix

--- src/mods/kcp/segment.test.ts
+++ src/mods/kcp/segment.test.ts
@@ -1,15 +1,12 @@
 import { Opaque, Readable, Writable } from "@hazae41/binary";
 import { Bytes } from "@hazae41/bytes";
 import { assert, test } from "@hazae41/phobos";
-import { webcrypto } from "crypto";
 import { relative, resolve } from "path";
 import { KcpSegment } from "./segment.js";
 
 const directory = resolve("./dist/test/")
 const { pathname } = new URL(import.meta.url)
 console.log(relative(directory, pathname.replace(".mjs", ".ts")))
-
-globalThis.crypto = webcrypto as any
 
 test("kcp segment", async ({ test }) => {
   const conversation = 12345
